c9c09ded857ee977de059180119eda87784b0cd7
FarajKhanjar <farajkhanjar@gmail.com>
2022-04-25 10:30:35 +0300

    Add intrestRate and fee fileds to class

--- AJBC_Bank_Project/src/AccountOwner/Account.java
+++ AJBC_Bank_Project/src/AccountOwner/Account.java
@@ -5,6 +5,8 @@ public class Account
 	protected double balance;
 	protected AccountProperties accountProperties;
 	protected ActivityData[] activityData;
+	private Double intrestRate; //TODO add it to class diagram.
+	private Double fee; //TODO add it to class diagram.
 
 	
 	public Account(double balance, AccountProperties accountProperties) 

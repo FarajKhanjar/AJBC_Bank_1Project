0a7ad1b8c525d260197a4bc055b772f8c519b52d
FarajKhanjar <farajkhanjar@gmail.com>
2022-04-25 20:31:54 +0300

    Check account balance.

--- AJBC_Bank_Project/src/AccountOwner/AccountOwner.java
+++ AJBC_Bank_Project/src/AccountOwner/AccountOwner.java
@@ -2,50 +2,93 @@ package AccountOwner;
 
 import BankManager.BankManager;
 import Person.Person;
+import Person.PhoneNumber;
+
 import java.time.LocalDate;
+//import java.util.Random;
 
 public class AccountOwner extends Person
 {
-	protected Account account = null;
-	protected double monthlyIncome;
+	//fields
+	private Account account = null;
+	private double monthlyIncome;
 	private Credentials credentials;
 	private BankManager bankManager;
 	
-	public AccountOwner(String firstName, String lastName, int areaCode, float number, LocalDate bitrthDate,
-			Account account, double monthlyIncome, Credentials credentials) {
-		super(firstName, lastName, areaCode, number, bitrthDate);
+	//constructor
+	public AccountOwner(String firstName, String lastName, PhoneNumber phoneNumber ,LocalDate bitrthDate,
+			Account account, double monthlyIncome, Credentials credentials, BankManager bankManager) 
+	{
+		super(firstName, lastName, phoneNumber, bitrthDate);
+		setAccount(account);
+		setMonthlyIncome(monthlyIncome);
+		setCredentials(credentials);
+		setManager(bankManager);
+	}
+
+
+	public Account getAccount() 
+	{
+		return account;
+	}
+		
+	private void setAccount(Account account) 
+	{
 		this.account = account;
+	}
+
+	public double getMonthlyIncome() 
+	{
+		return monthlyIncome;
+	}
+
+	private void setMonthlyIncome(double monthlyIncome) 
+	{
 		this.monthlyIncome = monthlyIncome;
+	}
+
+	public Credentials getCredentials() 
+	{
+		return credentials;
+	}
+
+	private void setCredentials(Credentials credentials) 
+	{
 		this.credentials = credentials;
 	}
-	
-	private void checkBalance()
-	{
 		
+	public BankManager getManager() 
+	{
+		return bankManager;
+	}
+
+	private void setManager(BankManager bankManager) 
+	{
+		this.bankManager = bankManager;
+	}
+	
+	public PhoneNumber getPhoneNumber() 
+	{
+		return super.getPhoneNumber();
+	}
+	
+	/**
+	 * Check account balance.
+	 */
+	public void checkBalance()
+	{
+		System.out.println("Balance: " + account.getBalance());
 	}
 	
 	private void produceReport(LocalDate start)
 	{
 		
 	}
-	
-	private void deposit()
-	{
-		
-	}
-	
-	private void withdrawal()
-	{
-		
-	}
+
 	
 	private void transferFunds()
 	{
 		
 	}
-	
-	public String getAccountUserName()
-	{
-		return credentials.getUserName();
-	}
+			
 }

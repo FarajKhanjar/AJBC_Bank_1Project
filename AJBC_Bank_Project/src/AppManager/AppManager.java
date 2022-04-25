e8410a022d996eb529bf791021050ca4853a3c73
FarajKhanjar <farajkhanjar@gmail.com>
2022-04-25 15:24:26 +0300

    finish the Login by a username&password + finish the Login by phoneNumber

--- AJBC_Bank_Project/src/AppManager/AppManager.java
+++ AJBC_Bank_Project/src/AppManager/AppManager.java
@@ -14,41 +14,128 @@ public class AppManager
 	
 	private AccountOwner currUser;
 	static  AccountOwner[] users;
-	users = new AccountOwner[100];
+	//users = new AccountOwner[100];
 
 	private BankManager bankManager;
 	private Person person;
 	private PhoneNumber phoneNumber;
 	
-	
-	private void login(String username, String password) 
+	//Constructor
+	public AppManager(AccountOwner currUser, AccountOwner[] users) 
 	{
-		int index = 1;
-		 // loop through file and check if we find a matching email and password
-	    while (index<=3) 
-	    {
-	        if (username.equals(users.getAccountUserName())) 
-	        {
-	            System.out.println("Logging in...");
-	            break;
-	        }
-	        index++;
-	    }
-		
+		setCurrUser(currUser);
+		setUsers(users);
 	}
-	private void login(PhoneNumber phoneNumber) 
-	{
 		
+		public AccountOwner getCurrUser() 
+		{
+			return currUser;
+		}
+
+
+		public void setCurrUser(AccountOwner currUser) 
+		{
+			this.currUser = currUser;
+		}
+
+		public AccountOwner[] getUsers() 
+		{
+			return users;
+		}
+
+		public void setUsers(AccountOwner[] users) 
+		{
+			for (int i = 0; i < users.length; i++) 
+			{
+				this.users[i]=users[i];
+			}
+		}
+		
+
+		
+		public static LocalDate createDate() 
+		{
+	        
+	        LocalDate date = LocalDate.of(scan.nextInt(), scan.nextInt(), scan.nextInt());
+	        //DateTimeFormat ("dd. MMM. yyyy");
+	        scan.close();
+	        return date;
+	    }		
+		
+		
+		
+	/**
+	 * A method for login to the banking system using username and password.
+	 * @param userName
+	 * @param password
+	 */
+	public void login(String userName, String password)
+	{
+		int index=0;
+		int counter=0; //The number of times the user tried to login to his account.
+		boolean checkUserName=false;
+		boolean checkPassword;
+	
+		do {
+			// System.out.println("Enter you Username please:");
+			// String username = scan.nextLine();
+			 for (index=0 ; index < users.length; index++) 
+			 {
+				checkUserName=users[index].getCredentials().getUserName().equals(userName);
+				if(checkUserName)
+					break;
+			 }
+			 
+		   } while(checkUserName);
+
+
+		do {
+			 //System.out.println("Enter a password");
+			 //String password = scan.nextLine();
+			 checkPassword=users[index].getCredentials().getPassword().equals(password);
+			 counter++;
+			} while(3>counter);
+
+			if(checkPassword)
+				setCurrUser(currUser);
+			else
+				System.out.println("For your safety, the account is locked for 30 minutes!");
+			
+			//Timer?????
+
 	}
 	
-	public static LocalDate createDate() 
+
+	
+	
+	/**
+	 * A method for login to the banking system using phone number.
+	 * @param phoneNumber
+	 */
+	public void login(PhoneNumber phoneNumber) 
 	{
-        
-        LocalDate date = LocalDate.of(scan.nextInt(), scan.nextInt(), scan.nextInt());
-//        DateTimeFormat ("dd. MMM. yyyy");
-        scan.close();
-        return date;
-    }
+		for (int i = 0; i < users.length; i++) 
+		{
+			if(users[i].getPhoneNumber() == phoneNumber) 
+			{
+				setCurrUser(currUser);
+				return;
+			}
+		}
+		System.out.println("Sorry.. Apparently there is a mistake, we do not have the phone number entered.");
+	}
+	
+
+	/**
+	 * A method for logout the system.
+	 */
+	private void logout() 
+	{
+		this.currUser=null;
+	}
+	
+	
+	
 	
 	/**
 	 * Actor: Applicant
@@ -56,6 +143,7 @@ public class AppManager
 	 */
 	private void OpenAccount() 
 	{
+		int i=0;
 		System.out.println("You are welcome to our AJBC Bank, please fill this application form:\n");
 		
 		System.out.println("Enter your first Name:v\n"
@@ -77,37 +165,30 @@ public class AppManager
 	     
 	    System.out.println("Enter the Password");
 	    String password = sc.nextLine();
-	    boolean checkUserNameLD = cheackLettersAndDigits(userName);  
-	    if(checkUserNameLD==false)
+	    
+	    boolean flagUsername=users[i].getCredentials().getUserName().equals(userName);
+	    if(flagUsername)
 	    {
-	         System.out.println("Invalid Username");
-	    }
-	    boolean checkPasswordLD = cheackLettersAndDigits(password);
-	    if(checkUserNameLD==false || password.length()>8 || password.length()<4)
-	    {
-	    	    System.out.println("Invalid Password");
-	    }
-	     
+	    	login(userName,password);
+            System.out.println("Welcome "+userName+" you have Logged-in Successfully");
+        }
+  
 		bankManager.setUsersToApprove(this.person);
-		
+	
 	}
 	
-	private boolean cheackLettersAndDigits(String userName)
-	{
-		if(userName.matches("[a-zA-Z0-9]*")) {
-			return true;
-		}
-		else
-			return false;
-	}
-	private void logout() 
-	{
-		
-	}
+	
 	
 	private static AccountOwner getOwnerByPhoneNum(PhoneNumber phoneNumber) 
 	{
 		return null;		
+	}
+	
+	private void fillsApplicationForm() 
+	{
+		System.out.println("Enter a Phone number");
+		currUser.setPhoneNumber(0);
+
 	}
 
 }

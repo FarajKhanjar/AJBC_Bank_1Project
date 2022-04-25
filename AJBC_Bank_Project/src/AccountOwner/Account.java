9b229eff6097e7033b19bddfbba95d38d08af14b
FarajKhanjar <farajkhanjar@gmail.com>
2022-04-25 21:08:57 +0300

    Add the depositCash methods

--- AJBC_Bank_Project/src/AccountOwner/Account.java
+++ AJBC_Bank_Project/src/AccountOwner/Account.java
@@ -1,16 +1,58 @@
 package AccountOwner;
 
+import java.time.LocalDateTime;
+
 public class Account 
 {
+	
 	protected double balance;
 	protected AccountProperties accountProperties;
-	protected ActivityData[] activityData;
+	protected ActivityData[] activityDataArray;	
 	private Double intrestRate; //TODO add it to class diagram.
-	private Double fee; //TODO add it to class diagram.
+	private Double operationFee; //TODO add it to class diagram.
+	private int index=0;
 	
-	public Account() 
+	public Account(double balance, AccountProperties accountProperties, Double intrestRate, Double operationFee) 
 	{
-
+		this.balance = balance;
+		this.accountProperties = accountProperties;
+		activityDataArray = new ActivityData[100];
+		this.intrestRate = intrestRate;
+		this.operationFee = operationFee;
+		setBalance(0); //empty account.
 	}
 	
+	private void setBalance(double balance) 
+	{
+		this.balance = balance;
+	}
+
+	public double getBalance()
+	{
+		return balance;
+	}
+	
+	public void deposit(int amount) 
+	{
+		setBalance(balance + amount);
+		activityOfData(ActivityName.DEPOSIT, "none",amount);
+	}
+	
+	private void activityOfData(ActivityName activityName, String info,double balanceChange) 
+	{
+		ActivityData activityData = new ActivityData(activityName, LocalDateTime.now() ,info ,balanceChange);
+		activityDataArray(activityData);
+	}
+	
+	private void activityDataArray(ActivityData activityData) 
+	{
+		if (activityDataArray.length <= index) 
+		{
+			System.out.println("Its the maximum array right now.");
+			return;
+		}
+		activityDataArray[index++] = activityData;
+	}
+
+
 }
--- AJBC_Bank_Project/src/AccountOwner/AccountOwner.java
+++ AJBC_Bank_Project/src/AccountOwner/AccountOwner.java
@@ -5,7 +5,8 @@ import Person.Person;
 import Person.PhoneNumber;
 
 import java.time.LocalDate;
-//import java.util.Random;
+import java.util.Random;
+import java.util.Scanner;
 
 public class AccountOwner extends Person
 {
@@ -14,6 +15,7 @@ public class AccountOwner extends Person
 	private double monthlyIncome;
 	private Credentials credentials;
 	private BankManager bankManager;
+	static  Scanner sc = new Scanner(System.in);
 	
 	//constructor
 	public AccountOwner(String firstName, String lastName, PhoneNumber phoneNumber ,LocalDate bitrthDate,
@@ -79,6 +81,49 @@ public class AccountOwner extends Person
 	{
 		System.out.println("Balance: " + account.getBalance());
 	}
+	
+	
+	/**
+	 * User makes a deposit directly to an ATM box using the app authentication code.
+	 */
+	public void deposit()
+	{
+		int authenticationCode = createATMcode();
+		//System.out.println("Your code is: " + authenticationCode);
+		System.out.print("Enter your authentication code to deposit cash: ");
+		int enterCode = sc.nextInt();
+
+		if (enterCode == authenticationCode) 
+		{
+			System.out.print("What is the amount of cash you want to deposit?");
+			int cashAmount = sc.nextInt();
+			account.deposit(cashAmount);
+			System.out.printf("Successful deposit of: "+cashAmount);
+		} 
+		else 
+		{
+			System.out.println("Your code incorrect.");
+		}
+	}
+
+	/**
+	 * In this method we interesting in get a random ATM code.
+	 * @return
+	 */
+	private int createATMcode() 
+	{
+		Random randNumCode = new Random();
+		int finalATMcode = 0;
+
+		for (int i = 0; i < 4; i++) {
+			finalATMcode *= 10;
+			finalATMcode += 1 + randNumCode.nextInt(9);
+		}
+		return finalATMcode;
+	}
+	
+	
+	
 	
 	private void produceReport(LocalDate start)
 	{

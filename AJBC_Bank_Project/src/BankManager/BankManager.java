388a410e309ee787257c82057a3d12beea03b336
FarajKhanjar <farajkhanjar@gmail.com>
2022-04-26 23:28:58 +0300

    Add Doucomation to BankManager class.

--- AJBC_Bank_Project/src/BankManager/BankManager.java
+++ AJBC_Bank_Project/src/BankManager/BankManager.java
@@ -8,13 +8,26 @@ import Person.PhoneNumber;
 import java.time.LocalDate;
 import java.util.Arrays;
 
+/**
+ * The BankManager class is create and give us a basic info of the bank manager from "Person" class,
+ * The bank manager are responsible of Approve users to enter the bank, and change details.
+ * @author Faraj
+ */
 public class BankManager extends AccountOwner
 {	
+	//Fields
 	public AccountOwner[] usersToApprove;
 	public int index = 0;
 	public AccountProperties accountProperties;
 			
-	
+	/**
+	 * Contractor
+	 * @param firstName of the BankManager
+	 * @param lastName of the BankManager
+	 * @param phoneNumber of the BankManager
+	 * @param birthDate of the BankManager , LocalDate.
+	 * @param credentials of the BankManager. userName and password.
+	 */
 	public BankManager(String firstName, String lastName, PhoneNumber phoneNumber,
 			LocalDate birthDate, Credentials credentials) 
 	{
@@ -24,18 +37,29 @@ public class BankManager extends AccountOwner
 			createTheManagerAccount();
 	}
 	
-
+	/**
+	 * Get an Set method help us to get values of the fields or set a new values.
+	 *
+	 */
 	public void setUsersToApprove(AccountOwner accountOwner) 
 	{
 		this.usersToApprove[index] = accountOwner;
 		index++;
 	}
 	
+	/**
+	 * The bank manager are responsible of Approve users to enter the bank,
+	 * @return in this method will return that users in the array of users.
+	 */
 	public AccountOwner[] getUsersToApprove() 
 	{
 		return usersToApprove;
 	}
 
+	/**
+	 * The bank manager are responsible of Approve users to enter the bank,
+	 * in this method will check each user in the array and approved a details in the account.
+	 */
 	public void setAndApproveAccount() 
 	{
 		for (int i = 0; this.usersToApprove[i]!=null; i++) 
@@ -47,27 +71,20 @@ public class BankManager extends AccountOwner
 		usersToApprove = null;
 	}
 	
+	/**
+	 * In this method we will create the BankManager account.
+	 */
 	private void createTheManagerAccount() 
 	{
-		Account account = new Account(500000d,AccountProperties.TITANIUM, 0, 0);
+		Account account = new Account(250000,AccountProperties.TITANIUM, 0, 0);
 		setAccount(account);
-	}
+	}	
 	
-	private void addUserToApprove(AccountOwner accountOwner) 
-	{
-		
-	}
-	
-	private void produceReport(LocalDate start) 
-	{
-		
-	}
-	
+	//Print a message of the info of the person = BankManager.
 	@Override
 	public String toString() 
 	{
 		return super.toString() + "BankManager [usersToApprove=" + ", index=" + index
 				+ ", accProperties=" + accountProperties + "]";
 	}
-
 }

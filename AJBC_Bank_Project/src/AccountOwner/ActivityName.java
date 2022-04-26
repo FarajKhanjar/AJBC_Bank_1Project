fa12dded43f0912afb135c74cbdf4b302585966b
FarajKhanjar <farajkhanjar@gmail.com>
2022-04-26 22:40:46 +0300

    Add Documentaion to Enum-ActivityName class.

--- AJBC_Bank_Project/src/AccountOwner/ActivityName.java
+++ AJBC_Bank_Project/src/AccountOwner/ActivityName.java
@@ -1,6 +1,11 @@
 package AccountOwner;
 
-public enum ActivityName 
+/**
+ * In this class we will show and use the coast values of the ActivityName.
+ * Its enum.
+ * @author Faraj
+ *
+ */public enum ActivityName 
 {	
 	MAKE_PAYMENT_TRANSFER,
 	PAY_BILL,

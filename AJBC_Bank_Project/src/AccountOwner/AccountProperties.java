ceadfd90c220cfa781cd1a7da5b2bad5e4811df2
FarajKhanjar <farajkhanjar@gmail.com>
2022-04-26 22:46:59 +0300

    Add Documentaion to Enam-AccountProperties class.

--- AJBC_Bank_Project/src/AccountOwner/AccountProperties.java
+++ AJBC_Bank_Project/src/AccountOwner/AccountProperties.java
@@ -1,16 +1,24 @@
 package AccountOwner;
 
+/**
+ * In this class we will get and use  the coast values of the Account Properties types.
+ * Its enum.
+ * @author Faraj
+ */
 public enum AccountProperties 
 {
+	//Basic info and types.
 	BRONZE(4.5f, 6f, 5f, 7.5f, 10000, 2500),
 	SILVER(3f, 4.5f, 3.8f, 5f, 20000, 4000),
 	GOLD(1.5f, 3f, 1.75f, 3.8f, 50000, 6000), 
 	TITANIUM(0, 0, 0, 0, 100000, 100000);
 
+	//Fields
 	double minInterestRate, maxInterestRate;
 	double minFee, maxFee;
 	double maxLoan, maxWithdrawal;
 
+	//Contractor
 	AccountProperties(double minInterestRate, double maxInterestRate, double minFee, double maxFee,
 		int maxLoanAmmount, int maxWithdrawal) {
 		this.minInterestRate = minInterestRate;
@@ -21,6 +29,11 @@ public enum AccountProperties
 		this.maxWithdrawal = maxWithdrawal;
 	}
 	
+	/**
+	 * using the SSD info, in this method we check if the account types is okay in relation monthlyIncome
+	 * @param monthlyIncome
+	 * @return
+	 */
 	public static AccountProperties properties(double monthlyIncome) 
 	{
 		if(monthlyIncome<5000)
@@ -31,6 +44,12 @@ public enum AccountProperties
 			return GOLD;
 		return TITANIUM;
 	}
+	
+	
+	/**
+	 * Get and Set method help us to get values of the fields or set a new values.
+	 *
+	 */	
 	
 	public double getIntresRateMin() 
 	{
